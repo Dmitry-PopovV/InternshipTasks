c479d2f09e7078a413dbf3c0c26ab4a8c841add3
Dmitry Popov <dmitry.popov.2002@gmail.com>
2023-02-03 11:29:03 -0300

    add tables creation

--- Question3.sql
+++ Question3.sql
@@ -1,4 +1,39 @@
 USE Task2;
+
+CREATE TABLE goods
+   (
+      id INT NOT NULL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+   );
+
+INSERT INTO goods (id, name) VALUES (0, 'Молоко');
+INSERT INTO goods (id, name) VALUES (1, 'Огурец');
+INSERT INTO goods (id, name) VALUES (2, 'Яблоко');
+INSERT INTO goods (id, name) VALUES (3, 'Стол');
+
+CREATE TABLE categories
+   (
+      id INT NOT NULL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+   );
+
+INSERT INTO categories (id, name) VALUES (0, 'Овощи');
+INSERT INTO categories (id, name) VALUES (1, 'Продукты');
+INSERT INTO categories (id, name) VALUES (2, 'Фрукты');
+
+CREATE TABLE goodsCategoriesLink
+   (
+      good_id INT NOT NULL,
+      category_id INT NOT NULL,
+	  CONSTRAINT PK_goodsCategoriesLink PRIMARY KEY (good_id, category_id)
+   );
+
+INSERT INTO goodsCategoriesLink (good_id, category_id) VALUES (0, 1);
+INSERT INTO goodsCategoriesLink (good_id, category_id) VALUES (1, 0);
+INSERT INTO goodsCategoriesLink (good_id, category_id) VALUES (1, 1);
+INSERT INTO goodsCategoriesLink (good_id, category_id) VALUES (2, 1);
+INSERT INTO goodsCategoriesLink (good_id, category_id) VALUES (2, 2);
+
 SELECT goods.name AS goodName, categories.name AS categoryName FROM goods 
 LEFT JOIN goodsCategoriesLink ON goods.id = goodsCategoriesLink.good_id 
 LEFT JOIN categories ON goodsCategoriesLink.category_id = categories.id;
